bf5dcd99d779642afc71277495fa78f09eea1f92
Azamat Almazbekov <azamatalmazbekov04@gmail.com>
2025-06-04 16:21:47 +0600

    added firebase services

--- build-logic/convention/src/main/kotlin/ApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/ApplicationConventionPlugin.kt
@@ -14,6 +14,8 @@ class ApplicationConventionPlugin : Plugin<Project> {
                 apply("com.android.application")
                 apply("org.jetbrains.kotlin.android")
                 apply("org.jetbrains.kotlin.plugin.compose")
+                apply("com.google.gms.google-services")
+                apply("com.google.firebase.crashlytics")
             }
 
             extensions.configure<ApplicationExtension> {
@@ -34,8 +36,7 @@ class ApplicationConventionPlugin : Plugin<Project> {
                 productFlavors {
                     create("dev") {
                         dimension = "environment"
-                        applicationIdSuffix = ".dev"
-                        versionNameSuffix = "-dev"
+                        applicationId = "com.almazbekov.safecoin"
 
                         // API настройки для разработки
                         buildConfigField("String", "API_BASE_URL", "\"https://api.sandbox.coinapi.io/v1/\"")
@@ -49,7 +50,7 @@ class ApplicationConventionPlugin : Plugin<Project> {
 
                     create("prod") {
                         dimension = "environment"
-
+                        applicationId = "com.safecoin.io"
                         // Продакшн API
                         buildConfigField("String", "API_BASE_URL", "\"https://rest.coinapi.io/v1/\"")
 
@@ -61,8 +62,6 @@ class ApplicationConventionPlugin : Plugin<Project> {
                 buildTypes {
                     getByName("debug") {
                         isDebuggable = true
-                        applicationIdSuffix = ".debug"
-                        versionNameSuffix = "-debug"
 
                         // Дополнительные debug настройки
                         buildConfigField("boolean", "IS_DEBUG", "true")
@@ -89,15 +88,18 @@ class ApplicationConventionPlugin : Plugin<Project> {
                 add("implementation", libs.findBundle("androidx-core").get())
 
                 // Compose dependencies
-                val bom = libs.findLibrary("androidx-compose-bom").get()
-                add("implementation", platform(bom))
+                val composeBom = libs.findLibrary("androidx-compose-bom").get()
+                add("implementation", platform(composeBom))
                 add("implementation", libs.findBundle("compose").get())
                 add("debugImplementation", libs.findLibrary("androidx-ui-tooling").get())
                 add("implementation", libs.findLibrary("kotlinx-datetime").get())
                 add("coreLibraryDesugaring", "com.android.tools:desugar_jdk_libs:2.0.4")
                 add("testImplementation", libs.findBundle("testing").get())
                 // Test dependencies
-                add("androidTestImplementation", platform(bom))
+                add("androidTestImplementation", platform(composeBom))
+                add("implementation", platform(libs.findLibrary("firebase-bom").get()))
+                add("implementation", libs.findLibrary("firebase-analytics").get())
+                add("implementation", libs.findLibrary("firebase-crashlytics").get())
             }
         }
     }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.detekt)
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.crashlytics) apply false
 }
 
 detekt {
