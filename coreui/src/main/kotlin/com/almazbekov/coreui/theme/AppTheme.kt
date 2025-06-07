3df6260289fe34bcb512bb7c150dad801feee553
Azamat Almazbekov <azamatalmazbekov04@gmail.com>
2025-06-07 19:45:19 +0600

    added some components and utils

--- coreui/src/main/kotlin/com/almazbekov/coreui/theme/AppTheme.kt
+++ coreui/src/main/kotlin/com/almazbekov/coreui/theme/AppTheme.kt
@@ -1,13 +1,11 @@
 package com.almazbekov.coreui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.staticCompositionLocalOf
 
 object AppTheme {
-    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     operator fun invoke(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
         CompositionLocalProvider(
